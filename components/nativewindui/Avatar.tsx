feb2cb028932f86f7df5e2cdcbdef6766f6a84b1
Zach Nugent <mrzachnugent@gmail.com>
2024-08-23 14:21:51 -0400

    refactor: use rn-primitives for avatar component

--- components/nativewindui/Avatar.tsx
+++ components/nativewindui/Avatar.tsx
@@ -1,123 +1,46 @@
+import * as AvatarPrimitive from '@rn-primitives/avatar';
 import * as React from 'react';
-import {
-  ImageErrorEventData,
-  ImageLoadEventData,
-  NativeSyntheticEvent,
-  Image as RNImage,
-  View,
-} from 'react-native';
 
 import { cn } from '~/lib/cn';
 
-interface AvatarRootProps {
-  alt: string;
-}
-
-interface AvatarImageProps {
-  children?: React.ReactNode;
-  onLoadingStatusChange?: (status: 'error' | 'loaded') => void;
-}
-
-type AvatarState = 'loading' | 'error' | 'loaded';
-
-interface IRootContext extends AvatarRootProps {
-  status: AvatarState;
-  setStatus: (status: AvatarState) => void;
-}
-
-const RootContext = React.createContext<IRootContext | null>(null);
-
 const Avatar = React.forwardRef<
-  React.ElementRef<typeof View>,
-  React.ComponentPropsWithoutRef<typeof View> & AvatarRootProps
->(({ alt, className, ...viewProps }, ref) => {
-  const [status, setStatus] = React.useState<AvatarState>('error');
+  React.ElementRef<typeof AvatarPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
+>(({ alt, className, ...props }, ref) => {
   return (
-    <RootContext.Provider value={{ alt, status, setStatus }}>
-      <View
-        ref={ref}
-        className={cn('relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full', className)}
-        {...viewProps}
-      />
-    </RootContext.Provider>
+    <AvatarPrimitive.Root
+      ref={ref}
+      alt={alt}
+      className={cn('relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full', className)}
+      {...props}
+    />
   );
 });
 
-Avatar.displayName = 'Avatar';
-
-function useRootContext() {
-  const context = React.useContext(RootContext);
-  if (!context) {
-    throw new Error('Avatar compound components cannot be rendered outside the Avatar component');
-  }
-  return context;
-}
+Avatar.displayName = AvatarPrimitive.Root.displayName;
 
 const AvatarImage = React.forwardRef<
-  React.ElementRef<typeof RNImage>,
-  Omit<React.ComponentPropsWithoutRef<typeof RNImage>, 'alt'> & AvatarImageProps
->(
-  (
-    { onLoad: onLoadProps, onError: onErrorProps, onLoadingStatusChange, className, ...props },
-    ref
-  ) => {
-    const { alt, setStatus, status } = useRootContext();
+  React.ElementRef<typeof AvatarPrimitive.Image>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
+>(({ className, ...props }, ref) => {
+  return (
+    <AvatarPrimitive.Image
+      ref={ref}
+      className={cn('aspect-square h-full w-full', className)}
+      {...props}
+    />
+  );
+});
 
-    React.useLayoutEffect(() => {
-      setStatus('loading');
-    }, []);
-
-    const onLoad = React.useCallback(
-      (e: NativeSyntheticEvent<ImageLoadEventData>) => {
-        setStatus('loaded');
-        onLoadingStatusChange?.('loaded');
-        onLoadProps?.(e);
-      },
-      [onLoadProps]
-    );
-
-    const onError = React.useCallback(
-      (e: NativeSyntheticEvent<ImageErrorEventData>) => {
-        setStatus('error');
-        onLoadingStatusChange?.('error');
-        onErrorProps?.(e);
-      },
-      [onErrorProps]
-    );
-
-    if (status === 'error') {
-      return null;
-    }
-
-    return (
-      <RNImage
-        ref={ref}
-        alt={alt}
-        onLoad={onLoad}
-        onError={onError}
-        className={cn('aspect-square h-full w-full', className)}
-        {...props}
-      />
-    );
-  }
-);
-
-AvatarImage.displayName = 'AvatarImage';
+AvatarImage.displayName = AvatarPrimitive.Image.displayName;
 
 const AvatarFallback = React.forwardRef<
-  React.ElementRef<typeof View>,
-  React.ComponentPropsWithoutRef<typeof View>
+  React.ElementRef<typeof AvatarPrimitive.Fallback>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
 >(({ className, ...props }, ref) => {
-  const { alt, status } = useRootContext();
-
-  if (status !== 'error') {
-    return null;
-  }
   return (
-    <View
+    <AvatarPrimitive.Fallback
       ref={ref}
-      role="img"
-      aria-label={alt}
       className={cn(
         'flex h-full w-full items-center justify-center rounded-full bg-muted',
         className
@@ -127,6 +50,6 @@ const AvatarFallback = React.forwardRef<
   );
 });
 
-AvatarFallback.displayName = 'AvatarFallback';
+AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName;
 
 export { Avatar, AvatarFallback, AvatarImage };
